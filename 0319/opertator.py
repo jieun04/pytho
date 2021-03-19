3a54b23742668bfd455d639b6abb603c6fd6bda9
jieun04 <80086204+jieun04@users.noreply.github.com>
2021-03-19 16:01:58 +0900

    Add files via upload

---
+++ 0319/opertator.py
@@ -0,0 +1,13 @@
+a = 3; b = ~a;
+print("a=",a,"b=", b, "b = b + 1", (b+1))
+
+a = 3; b = 5; c = a & b;
+print("a=",a,"b=",b, "c=",c)
+
+a = 3; b = 5; c = a ^ b;
+print("a=",a,"b=",b, "c=",c)
+
+a = 3; b = 5; c = a | b;
+print("a=",a,"b=",b, "c=",c)
+
+
