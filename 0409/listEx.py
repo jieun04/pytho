da55e2329f1a680b6943f5dcb2dc879c2e083d05
jieun04 <80086204+jieun04@users.noreply.github.com>
2021-04-09 15:51:33 +0900

    Add files via upload

---
+++ 0409/listEx.py
@@ -0,0 +1,11 @@
+fstName = ['M', 'i', 'n']
+sndName = ["Ji" , "Eun"] 
+stdNum = [2, 0, 2, 1, 0, 6, 0, 1, 7]
+
+print(fstName)
+print(sndName)
+#화면에 M, J, E를 출력하는 프로그램을 작성하시오
+print(fstName[0])       # M
+print(sndName[0][0])# J
+print(sndName[0][0][0])# E  
+                        
