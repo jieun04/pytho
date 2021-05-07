00dc409b24a64193782d63c77e4c09901b505843
jieun04 <80086204+jieun04@users.noreply.github.com>
2021-05-07 15:47:20 +0900

    Add files via upload

---
+++ 0507/scope.py
@@ -0,0 +1,14 @@
+def func1(a):
+    a = a + 5
+    print(a)
+    return a
+def func2(i):
+    print(b)
+    print(i)
+    
+a = int(input("정수입력"))
+print(a)
+b = func1(a)
+print(a)
+print(b)
+func2(6)
