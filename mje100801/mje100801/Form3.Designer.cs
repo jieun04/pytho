00fa5daf43f38ec4baf06671c7bc1b96e75d0278
jieun04 <80086204+jieun04@users.noreply.github.com>
2021-10-08 18:23:07 +0900

    Add files via upload

--- mje100801/mje100801/Form3.Designer.cs
+++ mje100801/mje100801/Form3.Designer.cs
@@ -29,7 +29,6 @@ namespace mje100801
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton3 = new System.Windows.Forms.RadioButton();
@@ -40,8 +39,6 @@ namespace mje100801
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // radioButton1
@@ -144,22 +141,11 @@ namespace mje100801
             this.label2.TabIndex = 9;
             this.label2.Text = "숙박기간";
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(14, 207);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(218, 159);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 10;
-            this.pictureBox1.TabStop = false;
-            // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
@@ -172,7 +158,6 @@ namespace mje100801
             this.Controls.Add(this.radioButton1);
             this.Name = "Form3";
             this.Text = "Form3";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -190,6 +175,5 @@ namespace mje100801
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
---
+++ mje100801/mje100801/Form4.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mje100801
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double pr = 0; //가격
+            double ge = 0; //수량
+            double to = 0; //주문금액
+
+            if(radioButton1.Checked == true)
+            {
+                //짜장면이 선택되어 있는 경우
+                pr = 4000;
+            }
+            else
+            {
+                if(radioButton2.Checked == true) 
+                {
+                    pr = 5000;
+                }
+                else
+                {
+                    if (radioButton3.Checked == true)
+                    {
+                        pr = 15000;
+                    }
+                    else
+                    {
+                        pr = 7000;
+                    }
+                }
+            }
+             if(textBox1.Text == "")
+            {
+                MessageBox.Show("수량을 선택하세요");
+                return;
+            }
+            ge = double.Parse(textBox1.Text);
+
+            if (ge >= 4)
+            {
+                to = pr * ge;
+                to = to * 0.9;
+            }
+            else
+            {
+                to = pr * ge;
+
+            }
+            textBox2.Text = to.ToString();
+        }
+    }
+}
